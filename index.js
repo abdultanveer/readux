0b5b900e27d9c0ccf4b27d161f40be54d45948cb
abdul <abdul.tanveer@gmail.com>
2024-08-08 14:41:07 +0530

    redux demo action dispatch

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('first')
+const { createStore } = require("redux")
+//action
+const BUY_CAKE = 'BUY_CAKE'
+
+//action creator
+function buyCake(){
+    return {
+        type:BUY_CAKE,
+        info:'buycake action'
+    }
+}
+
+const initialState = {
+    numCakes: 10,
+    numIcecreams:20
+}
+
+const reducer =(state = initialState,action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numCakes:state.numCakes -1
+        }
+        default : return state
+    }
+}
+
+const store = createStore(reducer)
+
+console.log('initial state is',store.getState())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+console.log('after dispatching state is',store.getState())
+
